b4be2b6b939080a3ac0f6459b04974a3c8c52831
Higor Alves <higorhaalves@gmail.com>
2021-12-17 17:58:03 -0300

    build: eslint rules

--- src/components/Geomtry/Ball.tsx
+++ src/components/Geomtry/Ball.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable */
 import React from 'react'
 
 import { MeshDistortMaterial, Sphere } from '@react-three/drei'
 
 export function Ball() {
 	return (
+		// @ts-ignore
 		<Sphere args={[1, 100, 200]} scale={2}>
+			{/*// @ts-ignore*/}
 			<MeshDistortMaterial
 				color={'#ffd43b'}
 				attach={'material'}
