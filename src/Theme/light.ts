4c9dece6dce429755cc17b4578838612918f9d7c
Higor Alves <higorhaalves@gmail.com>
2021-12-24 13:48:29 -0300

    fix: remove fonts

--- src/Theme/light.ts
+++ src/Theme/light.ts
@@ -1,10 +1,5 @@
 import { MantineThemeOverride } from '@mantine/core'
 
 export const lightTheme: MantineThemeOverride = {
-	colorScheme: 'light',
-	fontFamily: 'Biotif',
-	fontFamilyMonospace: 'Fira Code',
-	headings: {
-		fontFamily: 'Neuzeit Grotesk Bold'
-	}
+	colorScheme: 'light'
 }
