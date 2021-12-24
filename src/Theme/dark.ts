f1f434536e74fe9281ee1c39e6933cc43d2682f8
Higor Alves <higorhaalves@gmail.com>
2021-12-24 13:48:29 -0300

    fix: remove fonts

--- src/Theme/dark.ts
+++ src/Theme/dark.ts
@@ -1,10 +1,5 @@
 import { MantineThemeOverride } from '@mantine/core'
 
 export const darkTheme: MantineThemeOverride = {
-	colorScheme: 'dark',
-	fontFamily: 'Biotif',
-	fontFamilyMonospace: 'Fira Code',
-	headings: {
-		fontFamily: 'Neuzeit Grotesk Bold'
-	}
+	colorScheme: 'dark'
 }
