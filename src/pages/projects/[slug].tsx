36c26e151fb17b09c12b2a9a8766f546d0b630a4
Higor Alves <higorhaalves@gmail.com>
2021-12-30 11:45:28 -0300

    perf: seo for project page

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -22,7 +22,8 @@ import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
 import Image from 'next/image'
 
-import { Link, Technology, Title } from '~/components'
+import { Link, SEOHead, Technology, Title } from '~/components'
+import { Meta } from '~/layouts'
 import {
 	default as Contentful,
 	getProject,
@@ -97,87 +98,90 @@ type Props = {
 
 export default function Project({ project }: Props) {
 	return (
-		<Container>
-			<div style={{ borderRadius: '8px', overflow: 'hidden' }}>
-				<Image
-					src={project.cover.url}
-					width={1920}
-					height={1080}
-					layout={'responsive'}
-					alt={project.cover.title}
-					objectFit={'cover'}
-					objectPosition={'center'}
-				/>
-			</div>
-			<Title mt={'xl'} align={'center'} white>
-				{project.title as string}
-			</Title>
-
-			<MDXRemote {...(project.source as any)} components={components} />
-
-			<section>
-				<Space h={30} />
-				<Title order={2} mb={'sm'}>
-					Details
+		<>
+			<SEOHead meta={project.seo as Meta} />
+			<Container>
+				<div style={{ borderRadius: '8px', overflow: 'hidden' }}>
+					<Image
+						src={project.cover.url}
+						width={1920}
+						height={1080}
+						layout={'responsive'}
+						alt={project.cover.title}
+						objectFit={'cover'}
+						objectPosition={'center'}
+					/>
+				</div>
+				<Title mt={'xl'} align={'center'} white>
+					{project.title as string}
 				</Title>
-				<Grid>
-					<Col span={12} md={3} lg={3}>
-						<Group>
-							<ThemeIcon color='yellow' size={24} radius='xl'>
-								<RubyIcon size={12} />
-							</ThemeIcon>
-							<Text>{project.company}</Text>
-						</Group>
-					</Col>
-					<Col span={12} md={4} lg={3}>
-						<Group>
-							<ThemeIcon color='yellow' size={24} radius='xl'>
-								<OrganizationIcon size={12} />
-							</ThemeIcon>
-							<Text>{project.industry}</Text>
-						</Group>
-					</Col>
-					<Col span={12} md={4} lg={3}>
-						<Group>
-							<ThemeIcon color='yellow' size={24} radius='xl'>
-								<BriefcaseIcon size={12} />
-							</ThemeIcon>
-							<Text>{project.role}</Text>
-						</Group>
-					</Col>
-					<Col span={12} md={4} lg={3}>
-						<Group>
-							<ThemeIcon color='yellow' size={24} radius='xl'>
-								<LocationIcon size={12} />
-							</ThemeIcon>
-							<Text>{project.country}</Text>
-						</Group>
-					</Col>
-					<Col span={12} md={4} lg={3}>
-						<Group>
-							<ThemeIcon color='yellow' size={24} radius='xl'>
-								<GlobeIcon size={12} />
-							</ThemeIcon>
-							<Link href={project.site as string} target={'_blank'}>
-								<Text color={'yellow'}>EduLivre.org</Text>
-							</Link>
-						</Group>
-					</Col>
+
+				<MDXRemote {...(project.source as any)} components={components} />
+
+				<section>
+					<Space h={30} />
+					<Title order={2} mb={'sm'}>
+						Details
+					</Title>
+					<Grid>
+						<Col span={12} md={3} lg={3}>
+							<Group>
+								<ThemeIcon color='yellow' size={24} radius='xl'>
+									<RubyIcon size={12} />
+								</ThemeIcon>
+								<Text>{project.company}</Text>
+							</Group>
+						</Col>
+						<Col span={12} md={4} lg={3}>
+							<Group>
+								<ThemeIcon color='yellow' size={24} radius='xl'>
+									<OrganizationIcon size={12} />
+								</ThemeIcon>
+								<Text>{project.industry}</Text>
+							</Group>
+						</Col>
+						<Col span={12} md={4} lg={3}>
+							<Group>
+								<ThemeIcon color='yellow' size={24} radius='xl'>
+									<BriefcaseIcon size={12} />
+								</ThemeIcon>
+								<Text>{project.role}</Text>
+							</Group>
+						</Col>
+						<Col span={12} md={4} lg={3}>
+							<Group>
+								<ThemeIcon color='yellow' size={24} radius='xl'>
+									<LocationIcon size={12} />
+								</ThemeIcon>
+								<Text>{project.country}</Text>
+							</Group>
+						</Col>
+						<Col span={12} md={4} lg={3}>
+							<Group>
+								<ThemeIcon color='yellow' size={24} radius='xl'>
+									<GlobeIcon size={12} />
+								</ThemeIcon>
+								<Link href={project.site as string} target={'_blank'}>
+									<Text color={'yellow'}>EduLivre.org</Text>
+								</Link>
+							</Group>
+						</Col>
+					</Grid>
+				</section>
+
+				<Space h={30} />
+				<Title order={3} mb={'xl'}>
+					Technologies used
+				</Title>
+
+				<Grid gutter={20}>
+					{project.technologiesCollection?.items.map(tech => (
+						<Col span={12} md={1} lg={2} key={tech.name}>
+							<Technology icon={tech.icon} name={tech.name} />
+						</Col>
+					))}
 				</Grid>
-			</section>
-
-			<Space h={30} />
-			<Title order={3} mb={'xl'}>
-				Technologies used
-			</Title>
-
-			<Grid gutter={20}>
-				{project.technologiesCollection?.items.map(tech => (
-					<Col span={12} md={1} lg={2} key={tech.name}>
-						<Technology icon={tech.icon} name={tech.name} />
-					</Col>
-				))}
-			</Grid>
-		</Container>
+			</Container>
+		</>
 	)
 }
