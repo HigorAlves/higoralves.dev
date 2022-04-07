7d32f2bb83eb132191bbff4d5aaa0cd3a205ee01
Higor Alves <higorhaalves@gmail.com>
2022-04-06 17:02:29 -0700

    fix: unused lib
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- src/containers/About/PresentationComponent.tsx
+++ src/containers/About/PresentationComponent.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
 import { Space, Text } from '@mantine/core'
-import { useTranslation } from 'next-i18next'
 
 import { Title } from '~/components'
 
 export function PresentationComponent() {
-	const { t } = useTranslation('about')
-
 	return (
 		<>
 			<Title order={1} white>
