89941717d51f866cb3aa754f1d58f460e3d2ed74
Higor Alves <higorhaalves@gmail.com>
2023-02-04 17:45:02 -0300

    refactor: card creation
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- src/components/Card/Card.component.tsx
+++ src/components/Card/Card.component.tsx
@@ -1,27 +1,14 @@
-import {
-	ActionIcon,
-	Badge,
-	Button,
-	Card,
-	Group,
-	Image,
-	Text
-} from '@mantine/core'
-import { IconHeart } from '@tabler/icons'
+import { Badge, Card, Group, Image, Text } from '@mantine/core'
 
 import { useStyles } from './Card.styles'
 
-interface IBadge {
-	emoji: string
-	label: string
-}
-
-interface IBadgeCardProps {
+export interface IBadgeCardProps {
 	image: string
 	title: string
+	jobTitle: string
 	company: string
 	description: string
-	badges: Array<IBadge>
+	isFeature?: boolean
 }
 
 export function BadgeCard({
@@ -29,19 +16,9 @@ export function BadgeCard({
 	title,
 	description,
 	company,
-	badges
+	jobTitle
 }: IBadgeCardProps) {
-	const { classes, theme } = useStyles()
-
-	const features = badges.map(badge => (
-		<Badge
-			color={theme.colorScheme === 'dark' ? 'dark' : 'gray'}
-			key={badge.label}
-			leftSection={badge.emoji}
-		>
-			{badge.label}
-		</Badge>
-	))
+	const { classes } = useStyles()
 
 	return (
 		<Card withBorder radius='md' p='md' className={classes.card}>
@@ -51,33 +28,19 @@ export function BadgeCard({
 
 			<Card.Section className={classes.section} mt='md'>
 				<Group position='apart'>
-					<Text size='lg' weight={500}>
+					<Text size='lg' weight={500} m={0}>
 						{title}
 					</Text>
 					<Badge size='sm'>{company}</Badge>
 				</Group>
-				<Text size='sm' mt='xs'>
+				<Text size='sm' weight={300}>
+					{jobTitle}
+				</Text>
+
+				<Text size='sm' mt='xs' lineClamp={3}>
 					{description}
 				</Text>
 			</Card.Section>
-
-			<Card.Section className={classes.section}>
-				<Text mt='md' className={classes.label} color='dimmed'>
-					Perfect for you, if you enjoy
-				</Text>
-				<Group spacing={7} mt={5}>
-					{features}
-				</Group>
-			</Card.Section>
-
-			<Group mt='xs'>
-				<Button radius='md' style={{ flex: 1 }}>
-					Show details
-				</Button>
-				<ActionIcon variant='default' radius='md' size={36}>
-					<IconHeart size={18} className={classes.like} stroke={1.5} />
-				</ActionIcon>
-			</Group>
 		</Card>
 	)
 }
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,4 +1,18 @@
-import { Text, Title } from '@mantine/core'
+import { Grid, Space, Text, Title } from '@mantine/core'
+
+import { BadgeCard, IBadgeCardProps } from '~/components/Card/Card.component'
+
+const PROJECTS: Array<IBadgeCardProps> = [
+	{
+		title: 'Bubbles',
+		company: 'Notch Team',
+		jobTitle: 'Senior Software Engineer',
+		image:
+			'https://www.higoralves.dev/_next/image/?url=https%3A%2F%2Fmedia.graphassets.com%2Foutput%3Dformat%3Awebp%2FCQyHrCIRZCjsNnU8GeVA&w=3840&q=75',
+		description:
+			'Collaborate using screen, video, and audio messages so you can work on your own time. No account or installation needed'
+	}
+]
 
 export default function ProjectsPage() {
 	return (
@@ -18,6 +32,31 @@ export default function ProjectsPage() {
 					others have been discontinued
 				</Text>
 			</div>
+
+			<Space h={'xl'} />
+
+			<Title order={3} ta='left'>
+				Feature Projects
+			</Title>
+			<Space h={'xl'} />
+
+			<Grid>
+				{PROJECTS.map((project: IBadgeCardProps) => (
+					<Grid.Col key={project.title} sm={12} md={4} lg={4}>
+						<BadgeCard {...project} />
+					</Grid.Col>
+				))}
+			</Grid>
+
+			<Space h={60} />
+			<Title order={3} ta='left'>
+				All Projects
+			</Title>
+			<Text ta={'left'} size='xs' color='dimmed' weight={300}>
+				Some projects are under NDA so they cannot be listed.
+			</Text>
+			<Space h={'xl'} />
+			<Space h={'xl'} />
 		</>
 	)
 }
