03e0da775a9fe369069c7101d289861fb653f0bc
Higor Alves <higorhaalves@gmail.com>
2023-02-04 21:14:33 -0300

    chore: list of projects
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -37,6 +37,150 @@ export const PROJECTS: Array<IBadgeCardProps> = [
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'Tiled is a product that offers a customizable, collaborative workspace for teams to organize, prioritize, and track their work.'
+			'Hoag is a non-profit healthcare organization that operates hospitals and other healthcare facilities in Orange County, California.'
+	},
+	{
+		title: 'CIE Digital',
+		company: 'X-Team',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'Hoag is a non-profit healthcare organization that operates hospitals and other healthcare facilities in Orange County, California.'
+	},
+	{
+		title: 'Ivy Cash',
+		company: 'X-Team',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'XHQ Platform',
+		company: 'X-Team',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Consultant',
+		company: 'UNESCO',
+		jobTitle: 'Technology Consultant',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Telus',
+		company: 'X-Team',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Sherlock',
+		company: 'Nata House',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Toro Investimentos',
+		company: 'Toro',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'VideoPeel',
+		company: 'Nata House',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Monetus',
+		company: 'Monetus',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'AltoQI',
+		company: 'NKey',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Goodfood Market',
+		company: 'X-Team',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Resale',
+		company: 'Nata House',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Elderly Health Care',
+		company: 'FioCruz',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Gol a Gol',
+		company: 'Gol a Gol',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Educação Livre',
+		company: 'UNESCO',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Traders Club',
+		company: 'Traders Club',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Hacker Rangers',
+		company: 'Hacker Rangers',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+	},
+	{
+		title: 'Ita Fest',
+		company: 'Personal',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
 	}
 ]
