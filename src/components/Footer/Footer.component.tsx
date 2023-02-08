651a7c1f7d78489828ee701890e3a53c6c0efdc6
Higor Alves <higorhaalves@gmail.com>
2023-02-08 11:18:59 -0300

    refactor: separating styles from the component
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -1,36 +1,9 @@
-import { Anchor, createStyles, Group } from '@mantine/core'
+import { Anchor, Group } from '@mantine/core'
 import Link from 'next/link'
 
 import { Logo, SocialMediaLinks } from '~/components/Icons'
 
-const useStyles = createStyles(theme => ({
-	footer: {
-		gridArea: 'footer',
-		marginTop: 120,
-		borderTop: `1px solid ${
-			theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
-		}`,
-		background: theme.colors.dark[7]
-	},
-
-	inner: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		padding: `${theme.spacing.md}px ${theme.spacing.md}px`,
-
-		[theme.fn.smallerThan('sm')]: {
-			flexDirection: 'column'
-		}
-	},
-
-	links: {
-		[theme.fn.smallerThan('sm')]: {
-			marginTop: theme.spacing.lg,
-			marginBottom: theme.spacing.sm
-		}
-	}
-}))
+import { useStyles } from './Footer.styles'
 
 interface FooterCenteredProps {
 	links: { link: string; label: string }[]
