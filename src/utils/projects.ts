30eaac6a7b06319bf683e8d38e583cfe45be8b28
Higor Alves <higorhaalves@gmail.com>
2023-02-08 10:10:15 -0300

    chore: project descriptions
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -45,7 +45,7 @@ export const PROJECTS: Array<IBadgeCardProps> = [
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'Hoag is a non-profit healthcare organization that operates hospitals and other healthcare facilities in Orange County, California.'
+			'Venture studio that serves as an innovation lab and accelerator for bold entrepreneurs who want to transform disruptive ideas into thriving ventures.'
 	},
 	{
 		title: 'Ivy Cash',
@@ -61,31 +61,24 @@ export const PROJECTS: Array<IBadgeCardProps> = [
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+			'XHQ Platform is a internal platform to manage teams, financial and cultural part of X-Team, you can generate insides and challenges.'
 	},
-	{
-		title: 'Consultant',
-		company: 'UNESCO',
-		jobTitle: 'Technology Consultant',
-		isFeature: false,
-		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
-	},
+
 	{
 		title: 'Telus',
 		company: 'X-Team',
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+			'Telecommunications company that provides a wide range of communication and entertainment services to both residential and business customers. '
 	},
 	{
-		title: 'Sherlock',
-		company: 'Nata House',
+		title: 'Good food Market',
+		company: 'X-Team',
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+			'Is a platform that offers meal kit delivery service based in Canada, a convenient way to eat well without sacrificing taste or quality.'
 	},
 	{
 		title: 'Toro Investimentos',
@@ -93,15 +86,7 @@ export const PROJECTS: Array<IBadgeCardProps> = [
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
-	},
-	{
-		title: 'VideoPeel',
-		company: 'Nata House',
-		jobTitle: 'Senior Software Engineer',
-		isFeature: false,
-		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+			'An investment platform based in Brazil that provides a comprehensive suite of investment services and tools for individuals and businesses.'
 	},
 	{
 		title: 'Monetus',
@@ -109,55 +94,7 @@ export const PROJECTS: Array<IBadgeCardProps> = [
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
-	},
-	{
-		title: 'AltoQI',
-		company: 'NKey',
-		jobTitle: 'Senior Software Engineer',
-		isFeature: false,
-		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
-	},
-	{
-		title: 'Goodfood Market',
-		company: 'X-Team',
-		jobTitle: 'Senior Software Engineer',
-		isFeature: false,
-		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
-	},
-	{
-		title: 'Resale',
-		company: 'Nata House',
-		jobTitle: 'Senior Software Engineer',
-		isFeature: false,
-		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
-	},
-	{
-		title: 'Elderly Health Care',
-		company: 'FioCruz',
-		jobTitle: 'Senior Software Engineer',
-		isFeature: false,
-		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
-	},
-	{
-		title: 'Gol a Gol',
-		company: 'Gol a Gol',
-		jobTitle: 'Senior Software Engineer',
-		isFeature: false,
-		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
-	},
-	{
-		title: 'Educação Livre',
-		company: 'UNESCO',
-		jobTitle: 'Senior Software Engineer',
-		isFeature: false,
-		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+			'An investment platform based in Brazil that do all the job for their clients, auto investing based on AI that follows the user dreams.'
 	},
 	{
 		title: 'Traders Club',
@@ -165,7 +102,64 @@ export const PROJECTS: Array<IBadgeCardProps> = [
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+			'A community platform (Mobile & Web) for investor based in Brazil, you can follow the market, benchmark your investments.'
+	},
+	{
+		title: 'AltoQI',
+		company: 'NKey',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			"The company's suite of cloud-based software applications is designed to help organizations streamline and automate key business processes."
+	},
+	{
+		title: 'Sherlock',
+		company: 'Nata House',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'A platform to go beyond the time in app & total logins, get clear understanding of the true engagement of your user base, lifeblood at your SaaS.'
+	},
+	{
+		title: 'VideoPeel',
+		company: 'Nata House',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'Company that provides video marketing solutions for businesses, designed to help organizations create and manage video campaigns.'
+	},
+	{
+		title: 'Resale',
+		company: 'Nata House',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'Real estate outlet based in Brazil, with solutions to facilitate access to properties repossessed with financing operations.'
+	},
+
+	{
+		title: 'Elderly Health Care',
+		company: 'FioCruz',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'A program from the Brazilian Government, it`s a part of the Modular Training Program in Health care for the elderly.'
+	},
+	{
+		title: 'Consultant',
+		company: 'UNESCO',
+		jobTitle: 'Technology Consultant',
+		isFeature: false,
+		description:
+			'Member of the technical committee for the creation of courses and digital educational resources and struct for the inside platform.'
+	},
+	{
+		title: 'Educação Livre',
+		company: 'UNESCO',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'A platform that offers free, fun and applied education to the real world, especially for young people looking for a work and study opportunities.'
 	},
 	{
 		title: 'Hacker Rangers',
@@ -173,7 +167,7 @@ export const PROJECTS: Array<IBadgeCardProps> = [
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+			'A platform that promotes corporate cyber security culture through gamification, to meet the requirements of the LGPD.'
 	},
 	{
 		title: 'Ita Fest',
@@ -181,6 +175,14 @@ export const PROJECTS: Array<IBadgeCardProps> = [
 		jobTitle: 'Senior Software Engineer',
 		isFeature: false,
 		description:
-			'IVY allows anyone with a smart phone and web browser to accept NFTs, and a basket of other tokens, just by scanning a QR code.'
+			'A personal project develop to help people from my city to find the best areas and more secure for the Carnival Festival in Brazil.'
+	},
+	{
+		title: 'Gol a Gol',
+		company: 'Gol a Gol',
+		jobTitle: 'Senior Software Engineer',
+		isFeature: false,
+		description:
+			'A platform that helps soccer fields owners to administrate to rent and create subscriptions for local teams and matchs.'
 	}
 ]
