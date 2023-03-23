ee0aed94238a941dcc1526389d636caebc38a665
Higor Alves <higorhaalves@gmail.com>
2023-03-23 18:25:16 -0300

    chore: initial skills set

---
+++ src/types/projects.ts
@@ -0,0 +1,37 @@
+enum EmploymentType {
+	FULLTIME = 'Full-time'
+}
+
+enum LocationType {
+	REMOTE = 'Remote'
+}
+
+export type Skill = {
+	name: string
+	website: string
+	worksWithSince: Date
+	logo: {
+		light: string
+		dark: string
+	}
+}
+
+export type ProjectType = {
+	title: string
+	jobTitle: string
+	image: string
+	description: string
+	isFeature: boolean
+	employmentType: EmploymentType
+	skills: Array<Skill>
+	carousel: Array<string>
+	company: {
+		name: string
+		location: string
+		locationType: LocationType
+	}
+	duration: {
+		startDate: Date
+		endDate: Date
+	}
+}
---
+++ src/utils/skills.ts
@@ -0,0 +1,10 @@
+import { Skill } from '~/types/projects'
+
+export const skills: Array<Skill> = [
+	{
+		name: 'Javascript',
+		website: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+		image: '',
+		worksWithSince: new Date(2015, 0, 1)
+	}
+]
