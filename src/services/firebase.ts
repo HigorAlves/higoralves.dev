f444eddc3fd1d4aed5bb8ba1fa1b39f76e302328
Higor Alves <higorhaalves@gmail.com>
2023-04-27 09:13:32 -0300

    chore(app): adding firebase config

---
+++ src/services/firebase.ts
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, connectAuthEmulator } from 'firebase/auth'
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
+import { getStorage, connectStorageEmulator } from 'firebase/storage'
+
+import { FIREBASE_CONFIG } from '~/config'
+
+export default function firebase() {
+	const app = initializeApp(FIREBASE_CONFIG)
+	const firestore = getFirestore(app)
+	const storage = getStorage(app)
+	const auth = getAuth(app)
+
+	const isLocalHost = process.env.NODE_ENV === 'development'
+
+	if (isLocalHost) {
+		try {
+			connectAuthEmulator(auth, 'http://127.0.0.1:9099')
+			connectFirestoreEmulator(firestore, 'localhost', 8080)
+			connectStorageEmulator(storage, 'localhost', 9199)
+		} catch (e) {}
+	}
+
+	return {
+		firestore,
+		storage,
+		auth
+	}
+}
