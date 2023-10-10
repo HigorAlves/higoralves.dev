94e700eee66803778a12e59b1b19db22c7267b53
Higor Alves <me@higoralves.dev>
2023-10-10 19:46:58 -0300

    refactor: removing tailwind

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,5 @@
 import type { Preview } from '@storybook/react'
 
-import '~/assets/styles/tailwind.css'
-
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
