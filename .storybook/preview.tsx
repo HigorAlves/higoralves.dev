fdc6edd71c35468816e1a59172dbf9ff8672f3f4
Higor Alves <me@higoralves.dev>
2023-10-26 18:44:03 -0300

    chore: adding new preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,13 +2,13 @@
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css'
 
-import React, {useEffect} from 'react'
-import {addons} from '@storybook/preview-api'
-import {DARK_MODE_EVENT_NAME} from 'storybook-dark-mode'
-import {MantineProvider, useMantineColorScheme} from '@mantine/core'
+import React, { useEffect } from 'react'
+import { addons } from '@storybook/preview-api'
+import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode'
+import { MantineProvider, useMantineColorScheme } from '@mantine/core'
 
 // theme.ts file from previous step
-import {theme} from '../src/theme/theme'
+import { theme } from '../src/theme/theme'
 
 const channel = addons.getChannel()
 
